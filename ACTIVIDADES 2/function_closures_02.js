f200d041649b92ba242f2c7a41128a0836fd2221
CarlosFrancoCifuentes <72622430+CarlosFrancoCifuentes@users.noreply.github.com>
2021-11-08 20:27:19 +0100

    [ADD]: Ejercicios actividad 2

---
+++ ACTIVIDADES 2/function_closures_02.js
@@ -0,0 +1,11 @@
+var scope = "global scope";         // A global variable
+
+function checkscope() {
+    let scope = "local scope";      // A local variable
+    function f() { 
+        return scope;               // Return the value in scope here
+    }
+    return f;                   
+}
+
+console.log(checkscope()());        // Devolverá "local scope"
---
+++ ACTIVIDADES 2/function_hoisting_01.js
@@ -0,0 +1,13 @@
+/**
+ * Nada nuevo: hoisting de variables
+ * 
+ * Programming JavaScript Applications. Eric Elliott.
+ */
+
+var x = 1;
+function f() {
+    console.log(x);
+    var x = 2;
+};
+ 
+f(); // Devolverá 2
